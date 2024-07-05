3de6b0581fb8fc08134b0d41022de83b40264dd8
Marcos Magueta <maguetamarcos@gmail.com>
2024-07-05 20:42:03 -0300

    mnesia prototype

--- src/disk.erl
+++ src/disk.erl
@@ -23,28 +23,28 @@ keeper(HashTable) ->
         stop -> ok
     end.
 
-compose([{write, Filename, Content} | Tail]) ->
-    Hash = crypto:hash(sha256, Content),
-    FilesCommand = (fun (HashTable) ->
-                            Default = [],
-                            [C|Cs] = maps:get(Filename, HashTable, Default),
-                            maps:put(Filename, [[Hash|C],C|Cs], HashTable) end),
-    %% Write to mnesia (our WAL) here the commit to effectively change the file later
-    LocationsCommand = (fun (HashTable) ->
-                            case maps:get(Hash, HashTable, garbage) of
-                                garbage -> gotta_write;
-                                #{size := _, offset := _} -> ok
-                            end,
-                            maps:put(Filename, [[Hash|C],C|Cs], HashTable) end),
-    [{FilesCommand, LocationsCommand} | compose(Tail)].
+%% compose([{write, Filename, Content} | Tail]) ->
+%%     Hash = crypto:hash(sha256, Content),
+%%     FilesCommand = (fun (HashTable) ->
+%%                             Default = [],
+%%                             [C|Cs] = maps:get(Filename, HashTable, Default),
+%%                             maps:put(Filename, [[Hash|C],C|Cs], HashTable) end),
+%%     %% Write to mnesia (our WAL) here the commit to effectively change the file later
+%%     LocationsCommand = (fun (HashTable) ->
+%%                             case maps:get(Hash, HashTable, garbage) of
+%%                                 garbage -> gotta_write;
+%%                                 #{size := _, offset := _} -> ok
+%%                             end,
+%%                             maps:put(Filename, [[Hash|C],C|Cs], HashTable) end),
+%%     [{FilesCommand, LocationsCommand} | compose(Tail)].
 
 main() ->
     %% Files :: Filename => Hash list list
     FilesProc = spawn(?MODULE, keeper, [nothing]),
     %% Locations :: Hash => {size : int; offset: int}
     LocationsProc = spawn(?MODULE, keeper, [nothing]),
-    compose([{write, "blacklist", <<"catboys">>},
-             {write, "blacklist", <<"maidboys">>, <<"123">>}]),
+    %% compose([{write, "blacklist", <<"catboys">>},
+             %% {write, "blacklist", <<"maidboys">>, <<"123">>}]),
     ok.
 
 %% [[3],
--- src/server.erl
+++ src/server.erl
@@ -2,7 +2,19 @@
 
 -behaviour(application).
 
--export([start/2, stop/1, main/1]).
+-export([start/2, stop/1, main/1, bootstrap/0]).
+
+-record(files, {name, hashes}).
+-record(locations, {hash, offset, size}).
+
+bootstrap() ->
+    mnesia:create_schema([node()]),
+    mnesia:start(),
+    mnesia:create_table(files, [{attributes, record_info(fields, files)}]),
+    mnesia:create_table(locations, [{attributes, record_info(fields, locations)}]).
+
+%% insert_file(Name, Hashes) ->
+    %% mnesia:transaction(fun () -> mnesia:write(#files) end)
 
 start(_, _) ->
     io:fwrite("Hello World\n"),
