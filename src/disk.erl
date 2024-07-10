0214c565c65bf77baba9a899804d48eaeb2fde19
Marcos Magueta <maguetamarcos@gmail.com>
2024-07-09 23:12:30 -0300

    track hash appends and cursor

--- src/disk.erl
+++ src/disk.erl
@@ -34,44 +34,45 @@ keeper(HashTable) ->
         stop -> ok
     end.
 
-compose([{write, Filename, Content} | Tail]) ->
+create_command({write, Filename, Content}) ->
     Hash = crypto:hash(sha256, Content),
-    Command = 
-	(fun () ->
-		 case mnesia:read(files, Filename) of
-		     [] -> 
-			 mnesia:write(#files{name = Filename, hashes = [Hash]});
-		     [Latest|Rest] ->
-			 mnesia:write(#files{name = Filename, hashes = [[Hash|Latest],Latest|Rest]})
-		 end,
-		 case mnesia:read(locations, Hash) of
-		     [] -> case mnesia:read(cursor, Filename) of
-			       [] ->
-				   Location = #locations{hash = Hash, size = erlang:byte_size(Content), offset = 0},
-				   mnesia:write(Location);
-			       [Offset] ->
-				   Location = #locations{hash = Hash, size = erlang:byte_size(Content), offset = Offset},
-				   mnesia:write(Location)
-			   end;
-		     [_] -> already_registered
-		 end,
-		 case mnesia:read(cursor, Filename) of
-		     [] -> mnesia:write(#cursor{filename = Filename, offset = erlang:byte_size(Content)});
-		     [Cursor] -> mnesia:write(#cursor{filename = Filename, offset = Cursor#cursor.offset + erlang:byte_size(Content)})
-		 end
-	 end),
-    [Command | compose(Tail)];
+    (fun () ->
+	     case mnesia:read(files, Filename) of
+		 [] -> 
+		     mnesia:write(#files{name = Filename, hashes = [Hash]});
+		 [Latest|Rest] ->
+		     mnesia:write(#files{name = Filename, hashes = [[Hash|Latest],Latest|Rest]})
+	     end,
+	     case mnesia:read(locations, Hash) of
+		 [] -> case mnesia:read(cursor, Filename) of
+			   [] ->
+			       Location = #locations{hash = Hash, size = erlang:byte_size(Content), offset = 0},
+			       mnesia:write(Location);
+			   [Offset] ->
+			       Location = #locations{hash = Hash, size = erlang:byte_size(Content), offset = Offset},
+			       mnesia:write(Location)
+		       end;
+		 [_] -> already_registered
+	     end,
+	     case mnesia:read(cursor, Filename) of
+		 [] -> mnesia:write(#cursor{filename = Filename, offset = erlang:byte_size(Content)});
+		 [Cursor] -> mnesia:write(#cursor{filename = Filename, offset = Cursor#cursor.offset + erlang:byte_size(Content)})
+	     end
+     end).
 
-compose([]) -> [].
+compose([]) -> fun () -> nothing end;
+
+compose(Commands) ->
+    fun () -> lists:foreach(fun (F) -> F() end, lists:map(fun (X) -> create_command(X) end, Commands)) end.
 
 main() ->
     %% Files :: Filename => Hash list list
     %% FilesProc = spawn(?MODULE, keeper, [nothing]),
     %% Locations :: Hash => {size : int; offset: int}
     %% LocationsProc = spawn(?MODULE, keeper, [nothing]),
-    [Commands] = compose([{write, "blacklist", <<"catboys">>}]),
-             %% {write, "blacklist", <<"maidboys">>, <<"123">>}]),
-    Res = mnesia:transaction(Commands),
+    CommandBlock = compose([{write, "blacklist", <<"catboys">>}]),
+    %% {write, "blacklist", <<"maidboys">>, <<"123">>}]),
+    Res = mnesia:transaction(CommandBlock),
     io:format("~p\n", [Res]),
     ok.
 
